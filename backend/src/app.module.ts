37ef85f612e258349f87f2a9b3addbeb7ca02186
Tomán Viktor <viktor.toman19@gmail.com>
2025-11-14 10:46:37 +0100

    Initial commit
    Init backend & frontend & docker ecosystem

---
+++ backend/src/app.module.ts
@@ -0,0 +1,14 @@
+import {Module} from '@nestjs/common';
+import {AppController} from './app.controller';
+import {AppService} from './app.service';
+import {MongooseModule} from "@nestjs/mongoose";
+
+@Module({
+    imports: [
+        MongooseModule.forRoot(`mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@${process.env.MONGO_HOST}:${process.env.MONGO_PORT}/${process.env.MONGO_DB_NAME}?authSource=admin`)
+    ],
+    controllers: [AppController],
+    providers: [AppService],
+})
+export class AppModule {
+}
